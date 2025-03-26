0598eeec1f983c93c08e3bf155c59e2cecaedbe6
Miguel Gadelha <83650029+Miguel-Gadelha-Cavalcante@users.noreply.github.com>
2025-03-26 18:45:19 -0300

    Nova Configuração para a Atividade

--- backend/routes/imovel.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Exemplo de rota
-router.get("/", (req, res) => {
-  res.json({ message: "Lista de imóveis" });
-});
-
-module.exports = router;
